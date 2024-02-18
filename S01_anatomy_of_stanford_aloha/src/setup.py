8df8dc7648807bc376572801a02c0420695fe929
Kan Deng <kan.deng@qq.com>
2024-02-19 00:03:52 +0800

    Update setup.py

--- S01_anatomy_of_stanford_aloha/src/setup.py
+++ S01_anatomy_of_stanford_aloha/src/setup.py
@@ -1,1 +1,25 @@
+from setuptools import find_packages, setup
 
+package_name = 'stanford_aloha'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Kan Deng',
+    maintainer_email='kan.deng@qq.com',
+    description='The Stanford mobile aloha package for ROS2',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
