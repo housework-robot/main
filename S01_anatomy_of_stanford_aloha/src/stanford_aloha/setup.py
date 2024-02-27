8ee6e0e434fc5d656df49c615858bb95d5f3dfec
shaobodang <shaobo.dang@whaledynamic.com>
2024-02-27 14:51:38 +0800

    1. fix setup.py 2. add interbotix repos as submodules under src/third_party

--- S01_anatomy_of_stanford_aloha/src/stanford_aloha/setup.py
+++ S01_anatomy_of_stanford_aloha/src/stanford_aloha/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'stanford_aloha'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*launch')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
